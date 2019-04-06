b0ccdae34ca2e0360fdcb1040b70bd75fa42731c
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-04-06 00:33:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string.toLowerCase());
     return "I can't hear you!";
-  else if (string.toUpperCase());
+  } else if { (string.toUpperCase());
     return "YES INDEED!";
+  } else {
+    return "test";
+  }
 }
